484144265e0d597a811843acf3f8cdcae996409c
mauropagano <mauro.pagano@gmail.com>
2015-06-29 10:18:47 -0400

    Fixed dates :-)

--- sql/sqld360_0b_pre.sql
+++ sql/sqld360_0b_pre.sql
@@ -10,7 +10,7 @@ COL row_num FOR 9999999 HEA '#' PRI;
 
 -- version
 DEF sqld360_vYYNN = 'v1518';
-DEF sqld360_vrsn = '&&sqld360_vYYNN. (2015-06-17)';
+DEF sqld360_vrsn = '&&sqld360_vYYNN. (2015-06-28)';
 DEF sqld360_prefix = 'sqld360';
 
 -- get dbid
